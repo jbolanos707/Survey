57dc5a4cbe41d2c903e3ad3143ba88332ac8cdc2
jennifer & franz <student@epicodus.com>
2015-05-12 16:45:43 -0700

    can add survey and questions. can delete and update surveys

--- app.rb
+++ app.rb
@@ -38,7 +38,40 @@ post ('/surveys/:id') do
   redirect('/surveys/'.concat(survey_id.to_s))
 end
 
+delete ('/surveys/:id') do
+  @survey = Survey.find(params.fetch('id').to_i)
+  @survey.destroy()
+  redirect('/surveys')
+end
+
 get ('/questions/:id') do
-  @survey = Survey.find(params.fetch('id'))
+  @question = Question.find(params.fetch('id'))
+  survey_id = @question.survey_id
+  @survey = Survey.find(params.fetch('survey_id').to_i)
   erb(:question)
+end
+
+patch ('/questions/:id') do
+=begin
+  id = params.fetch("survey_id").to_i
+  question = Question.find(id)
+  update_question = params.fetch("content")
+  question.update({:content => content})
+  survey = question.
+
+  @survey = Survey.find(params.fetch('id'))
+  @question = Question.find(params.fetch('id'))
+  content = params.fetch("content")
+  @question.update(content: content)
+  erb(:question)
+end
+=end
+end
+
+delete ('/questions/:id') do
+  @survey = Survey.find(params.fetch('survey_id'))
+  @question = Question.find(params.fetch('id'))
+  survey_id = @question.survey_id
+  @question.destroy()
+  redirect('/surveys'.concat(survey_id.to_s))
 end
